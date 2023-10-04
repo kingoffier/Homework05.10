ad0f69ca06217a2be639fbb7aa89f627c9d34abd
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-10-04 23:35:37 +0500

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Введите смайлик");
+            string str = Console.ReadLine();
+            string value = @"^[;:]\S";
+            string value2 = @"-*\S";
+            string value3 = @"((\))|(\()|(\[)|(\]))\S";
+            if (Regex.Match(str, value).Success ) 
+            {
+               if (Regex.Match(str, value2).Success)
+                {
+                    
+                    if(Regex.Match(str, value3).Success)
+                    {
+                        Console.WriteLine("Это смайлик");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.ReadKey();
+        }
+    }
+}
